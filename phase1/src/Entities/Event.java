832fa9560c929dd13acf749c1d0eee4e8f118e3f
shanxinp <x.shan@mail.utoronto.ca>
2020-10-30 13:09:47 +0800

    Finished the content of Event Class

--- phase1/src/Entities/Event.java
+++ phase1/src/Entities/Event.java
@@ -1,4 +1,88 @@
 package Entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
+    private String title;
+    private LocalDateTime time;
+    private Room room;
+    private Speaker speaker;
+    private List<Attendee> attendeeList;
+
+    public Event(String eventTitle, LocalDateTime eventTime, Room room, Speaker speaker){
+    this.title = eventTitle;
+    this.time = eventTime;
+    this.room = room;
+    this.speaker = speaker;
+    this.attendeeList = new ArrayList<Attendee>(2);
+    }
+
+    /**
+     * Setter for title of the Event
+     * @param title  The title of this event
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Setter for time of the Event
+     * @param time  The time of this event
+     */
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    /**
+     * Setter for room of the Event
+     * @param room  The room that this event take place
+     */
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    /**
+     * Setter speaker of the Event
+     * @param speaker  The speaker of this event
+     */
+    public void setSpeaker(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    /**
+     * Getter for title of the Event
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Getter for room of the Event
+     */
+    public Room getRoom() {
+        return room;
+    }
+
+    /**
+     * Getter for speaker of the Event
+     */
+    public Speaker getSpeaker() {
+        return speaker;
+    }
+
+    /**
+     * Getter for time of the Event
+     */
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    /**
+     * Getter for the List of Attendee of the Event
+     */
+    public List<Attendee> getAttendeeList() {
+        return attendeeList;
+    }
 }
