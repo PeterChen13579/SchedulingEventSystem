f998eb2e32feb5e813fff9cbdfdb7fbc721081eb
Johnson Vo <johnson.vo24@mail.utoronto.ca>
2020-10-31 15:33:57 -0400

    Started LoginSystem

---
+++ phase1/src/Controllers/LogInSystem.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import java.io.Serializable;
+import UseCase.UserManager;
+
+/**
+ * How Users or any type are able to login into the app.
+ */
+public class LogInSystem implements Serializable {
+    UserManager um;
+    private boolean verified = false;
+    private int maxAttempts = 5; //the number of attempts, can be changed
+
+    /**
+     *
+     * @param enteredUsername The Username the User inputs
+     * @param enteredPassword The Password the User inputs
+     * @return Need to create the UI for the menus for this to make sense. Perhaps return the User type and call on
+     * the UI class?
+     */
+    public boolean verifyLogin(String enteredUsername, String enteredPassword) {
+        int i = 0;
+        while (i < maxAttempts) {
+            if (um.credentialAuthorization(enteredUsername, enteredPassword)) {
+                verified = true;
+            }
+        }
+        return verified; //Not sure what to return right now, need to see how the UI menu looks.
+    }
+}
