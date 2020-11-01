9e3eabe4d4bf5d80781ad399e168c9fa3a1062c9
Kailas Moon <kailasmoon@gmail.com>
2020-11-01 06:13:10 -0500

    Sending message to multiple users and getting chat with specified users

--- phase1/src/UseCase/ChatManager.java
+++ phase1/src/UseCase/ChatManager.java
@@ -6,6 +6,7 @@ import Entities.Message;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChatManager {
 
@@ -21,21 +22,64 @@ public class ChatManager {
     /**
      * Create a new chat and add it to the list of all chats
      * @param memberUsernames The users who are in the new chat
+     * @return The new chat
      */
-    public void createChat(List<String> memberUsernames) {
-        this.allChats.add(new Chat(memberUsernames));
+    public Chat createChat(List<String> memberUsernames) {
+        Chat newChat = new Chat(memberUsernames);
+        this.allChats.add(newChat);
+        return newChat;
     }
 
     /**
-     * Send a message
+     * Send a message to one chat
      * @param chat The chat that the message is being sent in
      * @param senderUsername The username of the sender
      * @param time The time the message was sent
      * @param content The content of the message
      */
-    public void sendMessage(Chat chat, String senderUsername, LocalDateTime time, String content) {
+    public void sendMessageToChat(Chat chat, String senderUsername, LocalDateTime time, String content) {
         Message message = new Message(senderUsername, time, content);
         chat.getAllMessages().add(message);
+    }
+
+    /**
+     * Send a message to a list of users
+     * @param usernames The usernames that the message is being sent to
+     * @param senderUsername The username of the sender
+     * @param time The time the message was sent
+     * @param content The content of the message
+     */
+    public void sendMessageToUsers(String[] usernames, String senderUsername, LocalDateTime time, String content) {
+
+        for (int i=0; i<usernames.length; i++) {
+            String[] thisChatUsernames = new String[] {senderUsername, usernames[i]};
+            Chat chat = this.getChatContainingUsers(thisChatUsernames); // Get the chat between the sender and the recipient
+            if (chat == null) {
+                chat = this.createChat(Arrays.asList(thisChatUsernames)); // If no such chat exists, create it
+            }
+            this.sendMessageToChat(chat, senderUsername, time, content);
+        }
+    }
+
+    /**
+     * Get the chat containing only the specified users. Returns null if no such chat exists
+     * @param usernames A list of the usernames of the users in the chat
+     * @return The chat containing all of the given users if it exists, or null otherwise
+     */
+    public Chat getChatContainingUsers(String[] usernames) {
+        for (int i=0; i<this.allChats.size(); i++) {
+            Chat chat = this.allChats.get(i);
+            boolean rightChat = true;
+            for (int j=0; j<usernames.length; j++) {
+                if (!chat.getMemberUsernames().contains(usernames[j])) {
+                    rightChat = false;
+                }
+            }
+            if (rightChat && chat.getMemberUsernames().size() == usernames.length) {
+                return chat;
+            }
+        }
+        return null;
     }
 
     /**
