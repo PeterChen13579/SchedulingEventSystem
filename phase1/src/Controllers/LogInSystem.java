df247f8deb4d1faaad6afd5df533ef59a95133e2
Johnson Vo <johnson.vo24@mail.utoronto.ca>
2020-11-09 13:28:15 -0500

    Created run method for LoginSystem to read input to login.

--- phase1/src/Controllers/LogInSystem.java
+++ phase1/src/Controllers/LogInSystem.java
@@ -1,6 +1,10 @@
 package Controllers;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import UseCase.UserManager;
 import Entities.User;
 
@@ -10,9 +14,30 @@ import Entities.User;
 public class LogInSystem implements Serializable {
     UserManager um;
     private boolean verified = false;
-    private int maxAttempts = 5; //the number of attempts, can be changed
 
-    /**
+    public void run() {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+        String input = "";
+        while (!input.equals("cancel")) {
+            System.out.println("Type 'Log In' to log in or 'cancel' to exit the program: ");
+            try {
+                input = br.readLine();
+                System.out.println("Please type in your username.: ");
+                String enteredUsername = br.readLine();
+                System.out.println("Please type in your password: ");
+                String enteredPassword = br.readLine();
+                if (verifyLogin(enteredUsername, enteredPassword)) {
+                    String username = enteredUsername;
+                    User userType = verifyUserType(username);
+                }
+            } catch (IOException e) {
+                System.out.println("Oops! Something unexpected happened!");
+                e.printStackTrace();
+            }
+        }
+    }
+        /**
      *
      * @param enteredUsername The Username the User inputs
      * @param enteredPassword The Password the User inputs
@@ -21,9 +46,13 @@ public class LogInSystem implements Serializable {
      */
     public boolean verifyLogin(String enteredUsername, String enteredPassword) {
         int i = 0;
+        int maxAttempts = 5; //the number of attempts, can be changed
         while (i < maxAttempts) {
             if (um.credentialAuthorization(enteredUsername, enteredPassword)) {
                 verified = true;
+            }
+            else {
+                i ++;
             }
         }
         return verified; //Not sure what to return right now, need to see how the UI menu looks.
@@ -33,8 +62,7 @@ public class LogInSystem implements Serializable {
      * When a User logs in, the system should recognize what kind of User they are and provide the appropriate menu
      * @return
      */
-    public User verifyUserType(String enteredUsername) {
+    public User verifyUserType(String username) {
         return null;
         }
-    }
 }
