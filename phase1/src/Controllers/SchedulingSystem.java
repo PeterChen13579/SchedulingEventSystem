1996848836fb8e41c84d32aae538a5ee39bd32b9
Lisa Chen <58052077+l13a@users.noreply.github.com>
2020-11-12 15:21:57 -0800

    Lisa: changed System.out.println to calling the presenter

--- phase1/src/Controllers/SchedulingSystem.java
+++ phase1/src/Controllers/SchedulingSystem.java
@@ -4,6 +4,7 @@ import Entities.User;
 import UseCase.EventManager;
 import UseCase.RoomManager;
 import UseCase.UserManager;
+import Presenters.UserMenu;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class SchedulingSystem {
     EventManager em;
     RoomManager rm;
     UserManager um;
+    private UserMenu menu = new UserMenu();
 
     /**
      * Constructor for SchedulingSystem
@@ -36,27 +38,27 @@ public class SchedulingSystem {
         String temp = "";//input is being added to temp
 
         while (!temp.equals("3")) {
-            System.out.println("Type '1' to add room; '2' to add new event; '3' to go back to main menu: ");
+            menu.printStatement("Type '1' to add room; '2' to add new event; '3' to go back to main menu: ");
             try {
                 temp = br.readLine();
                 if (temp.equals("1")) {
-                    System.out.println("Please enter the room number you want to add: ");
+                    menu.printStatement("Please enter the room number you want to add: ");
                     addRoom(br.readLine());
                 } else if (temp.equals("2")) {
-                    System.out.println("Please enter the date for the new event (in the format 'YYYYMMDD'): ");
+                    menu.printStatement("Please enter the date for the new event (in the format 'YYYYMMDD'): ");
                     String inputDate = br.readLine();
-                    System.out.println("Please enter the start time for the new event (24-hour time in the format 'HH:MM:SS'): ");
+                    menu.printStatement("Please enter the start time for the new event (24-hour time in the format 'HH:MM:SS'): ");
                     String inputTime = br.readLine();
-                    System.out.println("Please enter the room number for the new event: ");
+                    menu.printStatement("Please enter the room number for the new event: ");
                     String inputRoom = br.readLine();
-                    System.out.println("Please enter the speaker's username for the new event(case sensitive): ");
+                    menu.printStatement("Please enter the speaker's username for the new event(case sensitive): ");
                     String inputSpeaker = br.readLine();
-                    System.out.println("Please enter a unique title for the new event: ");
+                    menu.printStatement("Please enter a unique title for the new event: ");
                     String inputTitle = br.readLine();
                     addEvent(inputDate, inputTime, inputRoom, inputSpeaker, inputTitle);
                 }
             } catch (IOException e) {
-                System.out.println("Oops! Something unexpected happened!");
+                menu.printStatement("Oops! Something unexpected happened!");
             }
         }
     }
@@ -69,11 +71,11 @@ public class SchedulingSystem {
     public void addRoom(String rmNum){
         //check if room already exists
         if (rm.doesRoomExist(rmNum)){
-            System.out.println("Room already exists!");
+            menu.printStatement("Room already exists!");
         }else{
             //create the new room
             rm.createRoom(rmNum);
-            System.out.println("Room successfully created!");
+            menu.printStatement("Room successfully created!");
         }
     }
 
@@ -89,31 +91,31 @@ public class SchedulingSystem {
     public void addEvent (String date, String time, String rmNum, String speakerUsername, String title){
         //check if date is valid format and value
         if (!em.parseStringToLocalDate(date)){
-            System.out.println("Uh-oh! The date entered is not a valid date or not written in the correct format (YYYYMMDD)!");
+            menu.printStatement("Uh-oh! The date entered is not a valid date or not written in the correct format (YYYYMMDD)!");
         }
         //check if time is valid format and value
         else if (!em.parseStringToLocalTime(time)){
-            System.out.println("Uh-oh! The date entered is not a valid date or not written in the correct format (HH:MM:SS)!");
+            menu.printStatement("Uh-oh! The date entered is not a valid date or not written in the correct format (HH:MM:SS)!");
         }
         //check if room exists
         else if(!rm.doesRoomExist(rmNum)){
-            System.out.println("Uh-oh! Room does not exist! Please add this room first!");
+            menu.printStatement("Uh-oh! Room does not exist! Please add this room first!");
         }
         //check if room is booked already at this time
         else if(!em.isRoomAvailableAtTime(rmNum, date, time)){
-            System.out.println("Uh-oh! Room is already booked at the given time!");
+            menu.printStatement("Uh-oh! Room is already booked at the given time!");
         }
         //check if speaker exists
         else if(!um.doesSpeakerExist(speakerUsername)){
-            System.out.println("Uh-oh! Speaker does not exist! Please create an account for this speaker first!");
+            menu.printStatement("Uh-oh! Speaker does not exist! Please create an account for this speaker first!");
         }
         //check if speaker is already giving another talk at this time
         else if(!em.isSpeakerAvailableAtTime(date,time, speakerUsername)){
-            System.out.println("Uh-oh! The given speaker is already booked for another event at the given time!");
+            menu.printStatement("Uh-oh! The given speaker is already booked for another event at the given time!");
         }
         //check if event title is unique
         else if(!em.isEventTitleUnique(title)){
-            System.out.println("Uh-oh! The event title has already been taken!");
+            menu.printStatement("Uh-oh! The event title has already been taken!");
         }
         //if everything works out
         else if(canAddEvent(date, time, rmNum, speakerUsername, title)){
@@ -121,7 +123,7 @@ public class SchedulingSystem {
             em.createEvent(title, date, time, rmNum, speakerUsername);
             //update the speaker's list of events
             um.addEventToSpeaker(title, speakerUsername);
-            System.out.println("Event successfully created!");
+            menu.printStatement("Event successfully created!");
         }
     }
 
