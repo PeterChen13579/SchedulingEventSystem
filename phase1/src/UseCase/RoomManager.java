fabe39bef6d5928587b0b40a0a4c09d706c85bb6
Rui Miao <amyr.miao@mail.utoronto.ca>
2020-11-16 17:29:56 +0800

    Add documentation to methods in RoomManager

--- phase1/src/UseCase/RoomManager.java
+++ phase1/src/UseCase/RoomManager.java
@@ -12,17 +12,33 @@ import Entities.Room;
 public class RoomManager implements Serializable {
     private List<Room> allRoom;
 
+    /**
+     * A constructor for a RoomManager that will initialize the list of all room as an empty list.
+     */
     public RoomManager() {
         allRoom = new ArrayList<>();
     }
 
+    /**
+     * A constructor for a RoomManager that will initialize the list of all rooms as the given list.
+     * @param room a list of room.
+     */
     public RoomManager(List<Room> room) {
         allRoom = room;
     }
 
+    /**
+     * Create a new room.
+     * @param roomNum the room number of the new room.
+     */
     public void createRoom(String roomNum){ allRoom.add(new Room(roomNum));
     }
 
+    /**
+     * Check whether the room exists.
+     * @param roomNum the room number of the room that you want to check.
+     * @return true iff the room has existed. Otherwise, return false.
+     */
     public boolean doesRoomExist(String roomNum){
         for(Room room: allRoom){
             if(room.getRoomNum().equals(roomNum)) return true;
