195263cf38a84702ca6434c852e5ba8a7b039d7f
shanxinp <x.shan@mail.utoronto.ca>
2020-11-22 10:39:48 +0800

    Delete conference class

--- phase2/src/Entities/Conference.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-import java.io.Serializable;
-
-/**
- * A conference in our program that contains all functionalities specified by the phase 1 description.
- */
-public class Conference implements Serializable {
-}
