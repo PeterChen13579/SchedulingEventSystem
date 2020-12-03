77b4af320d0fb0540e5055257e166e928df0bd9a
William Wang <billwang115@gmail.com>
2020-12-03 16:50:41 -0500

    Designed it so that sending a message also unarchives the chat

--- phase2/src/Controllers/MessagingSystem.java
+++ phase2/src/Controllers/MessagingSystem.java
@@ -326,7 +326,6 @@ public class MessagingSystem {
         for (int i=0; i<archivedChats.size(); i++){
             UUID chatId = archivedChats.get(i);
             if (userChatManager.areNewMessages(userName, chatId)){ //removes chat from being unarchived if new messages are recieved
-                archivedChats.remove(chatId);
                 userChatManager.unarchiveChat(userName, chatId);
                 i -= 1;
             }
@@ -363,6 +362,9 @@ public class MessagingSystem {
                 this.userChatManager.sendImageMessageToChat(chat, senderUsername, time, content, imageString); //send an image/message
             } else {
                 this.userChatManager.sendMessageToChat(chat, senderUsername, time, content); //else just send a message normally
+            }
+            if (userChatManager.getArchivedChats(username).contains(chat)){  //unarchives chat if it is archived
+                userChatManager.unarchiveChat(senderUsername, chat);
             }
         }
     }
